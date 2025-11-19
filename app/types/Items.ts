240eb8f3df8c709a46faab19f72bc4c0b1e7706f
mohmmedlahlali20 <mohmmedlaeh81@gmaail.com>
2025-11-19 22:36:48 +0100

    feat(types): extend Item type with image field for card rendering

--- app/types/Items.ts
+++ app/types/Items.ts
@@ -1,4 +1,11 @@
 export interface Item {
     id: number | string
     name: string
+    data: dataItem
+    image: string
+}
+
+export interface dataItem {
+    color: string
+    capacity: string
 }
